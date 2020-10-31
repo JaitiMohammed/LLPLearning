a5ac0f57643b8d16a4daa527799dc9488981d940
jaitimohammed <jaitimohammed.gi@gmail.com>
2020-10-31 20:12:19 +0100

    Some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Landing } from "./components/layouts/Landing/Landing";
-import { Menu } from "./components/layouts/Navbar/Navbar";
-import About from "./pages/About";
-// import Login from "./components/Login/index";
-// import Team from "./pages/Team";
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Menu />
-        {/* <Route path='/login' component={Login} /> */}
-
-        <Landing />
-        <About />
-        {/* <Team /> */}
-      </Router>
       {/* Navbar */}
-      {/* Header Landing Page */}
-      {/* Section Services */}
-      {/* Section Last Posts */}
-      {/* Section Last Courses */}
+      {/* Hero Section */}
+      {/* Sections - About */}
+      {/* Sections - Services */}
+      {/* Latest Courses */}
+      {/* Latest Blog */}
+      {/* Sections - Team */}
+      {/* Sections - Contacts */}
       {/* Footer */}
     </div>
   );
