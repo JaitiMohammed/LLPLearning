3438be40d61ed919bc94d4b9b27356925669a5c8
jaitimohammed <jaitimohammed.gi@gmail.com>
2020-10-31 20:58:03 +0100

    Add Footer

---
+++ client/src/components/layouts/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Style.css";
+const Footer = () => {
+  return (
+    <footer id='sticky-footer' className='py-4  text-white-50'>
+      <div className='container text-center'>
+        <small>Copyright © LLPLearning 2020</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../../components/layouts/Footer/Footer";
 import { Menu } from "../../components/layouts/Navbar/Navbar";
 
 const Home = () => {
   return (
     <>
       <Menu />
+      <Footer />
     </>
   );
 };
